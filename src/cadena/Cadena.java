6be0e345800f2218cffbb7cc4555a7ff68f07ac8
Luis Felipe Evilla <luisfelipe588@hotmail.com>
2019-10-01 15:21:29 -0500

    finished project

---
+++ src/cadena/Cadena.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cadena;
+
+/**
+ *
+ * @author luisf
+ */
+public class Cadena {
+    public static void main(String[] args) {
+        GUI frame = new GUI();
+        frame.setVisible(true);
+        frame.setBounds(200,200, 280,220);
+    }
+    
+}
